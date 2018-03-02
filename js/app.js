bd2184359ac177d1a109995214ae78fbfd96771d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-02 22:56:34 +0530

    jQuery comparision with vanilla.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 // How we hide/show content in vanilla js
 
-// var box = document.querySelector('.box');
+// const box = document.querySelector('.box');
 // box.style.display = 'none';
 
 // How we do the same above thing in jQuery
 // jQuery('.box').hide();
 
-// jQuery provides a shorthand for jQuery functions in order to avoid extra typing extra time
-$('.box').hide();
-$('.box').show();
+// // jQuery provides a shorthand for jQuery functions in order to avoid extra typing extra time
+// $('.box').hide();
+// $('.box').show();
 
+// *********************************************//
 
+// Event Listening in vanilla.js & jQuery
+
+// box.addEventListener('click', function(){
+//   alert('You clicked me vanilla.js');
+// });
+
+$('.box').click(function(){
+  alert('You clicked me jQuery');
+});
