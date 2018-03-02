f1da16de91ede4beecd6f906432af28f04a426a1
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-02 22:48:40 +0530

    show & hide content

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
+// How we hide/show content in vanilla js
+
 // var box = document.querySelector('.box');
 // box.style.display = 'none';
+
+// How we do the same above thing in jQuery
+// jQuery('.box').hide();
+
+// jQuery provides a shorthand for jQuery functions in order to avoid extra typing extra time
+$('.box').hide();
+$('.box').show();
+
+
