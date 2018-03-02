fa807d287000f6e50fb2b498a0a0930c5b58e34d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-03 02:14:14 +0530

    blog ready without using any jQuery line for DOM manipulation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
-$('#flashMessage')
-  .hide()
-  .slideDown(1000)
-  .delay(3000)
-  .slideUp(1000);
+const message = document.getElementById('flashMessage');
+message.style.display = 'none';
 
-const title = "My First Blog Post";
-const content = "This is my first post!";
+// DOM Manipulation without using any jQuery Code
+const previewButton = document.getElementById('previewButton');
 
-$('#blogTitlePreview').text(title);
-$('#blogContentPreview').text(content);
+const titleInput = document.getElementById('blogTitleInput');
+
+const contentInput = document.getElementById('blogContentInput');
+
+const blogTitle = document.getElementById('blogTitlePreview');
+
+const blogContent = document.getElementById('blogContentPreview');
+
+
+previewButton.addEventListener('click', function(){
+  
+  if(!titleInput.value || !contentInput.value){
+    alert ('Please fill the required fields');
+  } else {
+    blogTitle.innerText = titleInput.value;
+  
+    blogContent.innerText = contentInput.value;
+
+    showMessage();
+
+  }
+
+  clearInputs();
+
+});
+
+function clearInputs() {
+  titleInput.value = "";
+  contentInput.value = "";
+
+}
+
+function showMessage() {
+  message.style.display = 'block';
+  setTimeout(function(){
+  message.style.display = "none";
+  }, 2000);
+}
