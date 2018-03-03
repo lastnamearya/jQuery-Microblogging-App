20ef6bcef4ba6d8ff4a853523dc46631dd12e704
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-03 06:17:02 +0530

    error alert implemented

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
 $('#flashMessage').hide();
 
-$('#previewButton').click(function() {
-  const title = $('#blogTitleInput').val();
-  const content = $('#blogContentInput').val();
+const title = $('#blogTitleInput').val();
+const content = $('#blogContentInput').val();
+let message = $('#flashMessage');
 
-  $('#blogTitlePreview').text(title);
-  $('#blogContentPreview').text(content);
+$('#previewButton').click(function() {
+  let title = $('#blogTitleInput').val();
+  let content = $('#blogContentInput').val();
+
+  if(!title || !content) {
+
+    message.text("Please fill both the required fields first");
+    message.css('background-color', 'red');
+
+    clearFields();
+
+  } else {
+    $('#blogTitlePreview').text(title);
+    $('#blogContentPreview').text(content);
+
+    clearFields();
+
+  }
 
   $('#flashMessage')
     .hide()
     .slideDown(1000)
-    .delay(3000)
+    .delay(1000)
     .slideUp(1000)
 });
+
+function clearFields(){
+  $('#blogTitleInput').val('');
+  $('#blogContentInput').val('');
+}
